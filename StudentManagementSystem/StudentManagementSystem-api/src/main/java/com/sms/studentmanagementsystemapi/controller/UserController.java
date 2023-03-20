630c07e53c9db3b7e1dd49f62963650e09560786
AduwaGeethanjana <104964449+AduwaGeethanjana@users.noreply.github.com>
2023-03-20 22:15:32 +0530

    .
    .

--- StudentManagementSystem/StudentManagementSystem-api/src/main/java/com/sms/studentmanagementsystemapi/controller/UserController.java
+++ StudentManagementSystem/StudentManagementSystem-api/src/main/java/com/sms/studentmanagementsystemapi/controller/UserController.java
@@ -1,29 +1,24 @@
-package com.sms.studentmanagementsystemapi.controller;
-
-
-import com.sms.studentmanagementsystemapi.exception.UserNotFoundException;
-import com.sms.studentmanagementsystemapi.model.User;
-import com.sms.studentmanagementsystemapi.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class UserController {
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
 
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping("/user")
     User newUser(@RequestBody User newUser) {
+        logger.trace("New student is Added");
         return userRepository.save(newUser);
 
     }
 
     @GetMapping("/users")
     List<User> getAllUsers(){
+        logger.trace("List of all Students");
         return userRepository.findAll();
     }
 
@@ -37,18 +32,20 @@ public class UserController {
 
     @PutMapping("/user/{id}")
     User updateUser(@RequestBody User newUser,@PathVariable Long id){
-       return userRepository.findById(id)
-               .map(user -> {
-                   user.setUsernames(newUser.getUsernames());
-                   user.setName(newUser.getName());
-                   user.setEmail(newUser.getEmail());
-                   user.setDegreeProgram(newUser.getDegreeProgram());
-                   return userRepository.save(user);
-               }).orElseThrow(()->new UserNotFoundException(id));
+        logger.trace("Student Details Updated");
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsernames(newUser.getUsernames());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    user.setDegreeProgram(newUser.getDegreeProgram());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
     }
 
     @DeleteMapping("/user/{id}")
     String deleteUser(@PathVariable Long id){
+        logger.trace("Student Deleted");
         if(!userRepository.existsById(id)){
             throw new UserNotFoundException(id);
         }
